e7f28b36e054b5bc7a78d06a1f4119111e70d8cd
Santiago Penagos Cordoba <santipenagos93@gmail.com>
2021-09-30 20:26:24 -0500

    first commit

---
+++ validaciones.js
@@ -0,0 +1,21 @@
+let registros = [];
+
+function agregarRegistro() {
+    objeto = {usuario:document.getElementById("nombre_usuario").value,
+    anoNacimiento:document.getElementById("anoNacimiento_usuario").value,
+    contrasena:document.getElementById("contrasena_usuario").value};
+    registros.push(objeto);
+    console.log(registros);
+    return(registros);
+}
+
+function EncontrarUsuarioPorEdad(arreglo) {
+    arreglo.reverse();
+    let ordenar = arreglo.sort((a, b) => a.anoNacimiento - b.anoNacimiento);
+    document.getElementById("arreglo").innerHTML = "El usuario con más edad es: " + ordenar[0];
+    return(ordenar[0]);
+}
+
+// module.exports.registros = registros;
+// module.exports.EncontrarUsuarioPorEdad = EncontrarUsuarioPorEdad;
+// module.exports.agregarRegistro = agregarRegistro;
